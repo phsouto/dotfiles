27794d7ddfb0c7244ed2359f62bb4b532c9d7aa6
Pedro H. G. Souto <phgsouto@gmail.com>
2019-03-20 20:38:04 -0300

    Adicionando script de pos-instalação do fedora

---
+++ fedora_post_install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Este script deve ser executado como root...
+if [ "$(id -u)" != "0" ]; then
+   echo "Este script deve ser executado como root ou sudo" 1>&2
+   exit 1
+fi
+
+
+# 'Adicionando repositório RPMFusion free...'
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
+
+# 'Adicionando repositório RPMFusion non-free...'
+dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# 'Instalado ferramentas básicas...'
+dnf install -y nano vim htop 
+
+# 'Codecs...'
+dnf install -y gstreamer1-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} --setopt=strict=0
+dnf install -y gstreamer1-{plugin-crystalhd,ffmpeg,plugins-{good,ugly,bad{,-free,-nonfree,-freeworld,-extras}{,-extras}}} libmpg123 lame-libs --setopt=strict=0
+
+# 'Instalando ferramentas extras...'
+dnf install -y vlc chromium qbittorrent gnome-tweaks
+dnf install -y neofetch
+dnf install -y fortune
+dnf install -y unrar
+dnf install -y tlp
+dnf install -y lm_sensors
+
+# Ativando o tlp
+systemctl enable tlp
+systemctl start tlp
+
+# Detectando os sensores
+sensors-detect --auto
+
+# 'Configurando o /etc/hosts'
+cp /etc/hosts /etc/hosts.orig \
+    && curl https://someonewhocares.org/hosts/hosts > /tmp/hosts \
+    && mv /tmp/hosts /etc/hosts
+
